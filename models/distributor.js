7ea19644d4587e511adb7358989a8a1e72e30f02
Daniel Valdebenito <dani270486@gmail.com>
2017-06-06 15:05:07 -0400

    Creacion coleccion Zona y correccion tipos de datos

--- models/distributor.js
+++ models/distributor.js
@@ -1,15 +1,15 @@
 'use strict'
-
+/* Modelo de distribuidor */
 var mongoose = require('mongoose');
 var timestamp = require('mongoose-timestamp')
 var Schema = mongoose.Schema;
 var DistributorSchema = Schema({
-    name: string,
-    nit: string,
-    email: string,
-    contact: string,
-    phone: string,
-    image: string
+    name: String,
+    nit: String,
+    email: String,
+    contact: String,
+    phone: String,
+    image: String
 });
 DistributorSchema.plugin(timestamp);
 module.exports = mongoose.model('Distributor', DistributorSchema);
---
+++ models/zone.js
@@ -0,0 +1,14 @@
+'use strict'
+/* Modelo de Zonas */
+var mongoose = require('mongoose');
+var timestamp = require('mongoose-timestamp')
+var Schema = mongoose.Schema;
+var ZoneSchema = Schema({
+    name: { type: String, unique: true },
+    points: [{
+        lat: Number,
+        lng: Number
+    }]
+});
+ZoneSchema.plugin(timestamp);
+module.exports = mongoose.model('Zone', ZoneSchema);
