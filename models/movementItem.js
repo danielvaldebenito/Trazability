218d34ab8362b3ae898588e352f2f1c3d8bb7c4f
Daniel Valdebenito <dani270486@gmail.com>
2017-06-30 14:38:50 -0400

    Creacion hook after save MOVEMENT ITEM. Consolidacion de ubicaciones de productos

--- models/movementItem.js
+++ models/movementItem.js
@@ -14,5 +14,32 @@ var MovementItemSchema = Schema({
 MovementItemSchema.plugin(timestamp)
 
 
+var Stock = require('./stock')
+// Triggers
+MovementItemSchema.post('save', (doc) => {
+    var product = doc.product
+    var warehouse = doc.movement.warehouse
+    var io = doc.movement.type
+    if(io == 'E')
+    {
+        Stock.findOne({ product: product, warehouse: warehouse }, (err, stock) => {
+            if(err) throw err
+            if(!stock)
+            {
+                var st = new Stock({
+                    warehouse: warehouse,
+                    product: product
+                })
+                st.save()
+            }
+        })
+    } else { // 'S'
+        Stock.findOneAndRemove({ product: product, warehouse: warehouse }, (err, removed) => {
+            if(err) throw err
+        })
+    }
+        
+    
+})
 
 module.exports = mongoose.model('MovementItem', MovementItemSchema)
