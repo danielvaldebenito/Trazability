91d51be32c095d9a5a65c44f15714f5f56d2c6b5
Daniel Valdebenito <dani270486@gmail.com>
2017-07-06 16:03:53 -0400

    Correccion error response device/register

--- controllers/device.js
+++ controllers/device.js
@@ -40,7 +40,7 @@ function registerDevice(req, res) {
                 if(!devStored) return res.status(404).send({done: false, code: 3, data: null, message: 'Error al guardar PDA'})
                 
                 return res.status(200)
-                            .send({done: false, code: 0, data: device, message: 'OK'})
+                            .send({done: false, code: 0, data: devStored, message: 'OK'})
             })
         } else {
             // actualizar
@@ -52,7 +52,7 @@ function registerDevice(req, res) {
                 if(!updatedDevice) return res.status(404).send({done: false, code: 6, data: null, message: 'Error al actualizar PDA'})
                 
                 return res.status(200)
-                            .send({done: false, code: 0, data: device, message: 'OK'})
+                            .send({done: false, code: 0, data: updatedDevice, message: 'OK'})
             })
         }
 
