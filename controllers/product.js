134f5e8dbb947cdff115bb100512668ccfacf65b
Daniel Valdebenito <dani270486@gmail.com>
2017-10-30 15:18:32 -0300

    Create false nif, query and get json file

--- controllers/product.js
+++ controllers/product.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const Product = require('../models/product')
+const ProductType = require('../models/productType')
 const Stock = require('../models/stock')
 const MovementItem = require('../models/movementItem')
 const Warehouse = require('../models/warehouse')
@@ -10,7 +11,8 @@ const Store = require('../models/store')
 const Decrease = require('../models/decrease')
 const InternalProcess = require('../models/internalProcess')
 const config = require('../config')
-
+const fs = require('fs')
+const path = require('path')
 function getOneByNif (req, res) {
     const nif = req.params.nif
     const limit = req.query.limit || 10
@@ -80,6 +82,94 @@ function getOneByNif (req, res) {
     })
 
 
+}
+function existsByNif (req, res) {
+    const nif = req.params.nif
+    Product.findOne({ $or: [{ nif: nif}, {formatted: nif}] }, (err, product) => {
+        if(err) return res.status(500).send({ exists: false, message: 'Ha ocurrido un error', err})
+        return res.status(200).send({ exists:  product != null })
+    })
+}
+const productService = require('../services/product')
+function createFalseNifs (req, res) {
+    ProductType.find((err, pts) => {
+        if(err) return res.status(500).send({done: false, message: 'Ha ocurrido un error al buscar tipos de producto', err})
+        let ids = pts.map((pt, index) => { return pt._id })
+        for (let i = 0; i < 100000; i++) {
+            let nif = '99-999999-' + i;
+            let random = Math.floor(Math.random() * pts.length - 1) + 1
+            productService.formatNif(nif)
+                .then(formatted => {
+                    let productType = ids[random];
+                    const product = new Product ({
+                        nif: formatted,
+                        formatted: formatted,
+                        productType: productType,
+                        enabled: true,
+                        createdByPda: false,
+                        createdBy: 'admin' 
+                    })
+                    product.save((err, saved) => {
+                        if(err) return res.status(500).send({ done: false, message: 'Ha ocurrido un error', err})
+                        if(i == 99999) {
+                            return res.status(200).send({ message: 'OK'})
+                        }
+                    })
+                })
+        }
+    })
+    
+}
+function getAllProducts (req, res) {
+    Product
+    .find({ enabled: true })
+    .select(['-_id','formatted', 'productType'])
+    .populate({ 
+        path: 'productType',
+        select: ['code']
+    })
+    .exec((err, products) => {
+        res.status(200).send(products)
+    })
+
+}
+function getAllProductsFormat (req, res) {
+    Product
+    .find({ enabled: true })
+    .select(['-_id','formatted', 'productType'])
+    .populate({ 
+        path: 'productType',
+        select: ['code']
+    })
+    .exec((err, products) => {
+        const data = products.map((product, index) => {
+            return product.formatted + '|' + product.productType.code
+        })
+        let date = new Date()
+        let day = date.getDate()
+        let month = date.getMonth() + 1;
+        let year = date.getFullYear();
+        let today = day + '-' + month + '-' + year;
+        fs.writeFileSync('./uploads/products/products-' + today + '.json', JSON.stringify(data))
+        return res.status(200).send({ done: true, message: 'OK, archivo escrito'})
+    })
+
+}
+
+function getJsonProducts (req, res){
+    let date = new Date()
+    let day = date.getDate()
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    let today = day + '-' + month + '-' + year;
+    const filePath = './uploads/products/products-' + today + '.json'
+    fs.exists(filePath, (exists) => {
+        if(exists) {
+            return res.sendFile(path.resolve(filePath))
+        } else {
+            return res.status(400).send({message: 'Archivo no existe'})
+        }
+    })
 }
 
 function getWarehouseByType(id) {
@@ -175,4 +265,11 @@ function getWarehouseByType(id) {
    return p;
 }
 
-module.exports = { getOneByNif }
+module.exports = { 
+    getOneByNif, 
+    existsByNif, 
+    createFalseNifs, 
+    getAllProducts, 
+    getAllProductsFormat,
+    getJsonProducts
+}
--- routes/product.js
+++ routes/product.js
@@ -4,7 +4,10 @@ const ProductController = require('../controllers/product')
 const md_auth = require('../middlewares/authenticated')
 const api = express.Router()
 
-
+api.get('/products', md_auth.ensureAuth, ProductController.getAllProducts)
+api.get('/products/format', md_auth.ensureAuth, ProductController.getAllProductsFormat)
+api.get('/products/json', md_auth.ensureAuth, ProductController.getJsonProducts)
 api.get('/product/:nif', md_auth.ensureAuth, ProductController.getOneByNif)
-
+api.get('/product-exists/:nif', md_auth.ensureAuth, ProductController.existsByNif)
+api.post('/product-false-nif', md_auth.ensureAuth, ProductController.createFalseNifs)
 module.exports = api;
